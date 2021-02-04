858560adff7e5c07620e45aabe13a52241696219
sergiop22 <sergiopadillan97@gmail.com>
2021-02-03 20:01:19 -0600

    commit

---
+++ Proyecto/urls.py
@@ -0,0 +1,34 @@
+"""Proyecto URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('index/', views.index, name="index"),
+    path('register/', views.register, name="register"),
+    path('login/', views.login_request, name="login"),
+    path('logout/', views.logout_request, name="logout"),
+]
+
+#configuración para cargar imagenes
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ main/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+from .models import Perfil
+from .forms import SignUpForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'layouts/home.html')
+
+def index(request):
+    return render(request, 'layouts/layout.html')
+
+def register(request):
+    if request.method =="POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            usuario = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            usuario = authenticate(username=usuario, password=password)
+            login(request, usuario)
+            return redirect("index")
+        else:
+            print("Error")
+
+    form = SignUpForm()
+    return render(request, "auth/register.html", {"form": form})
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=usuario, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("index")
+
+            else:
+                messages.error(request, "Usuario o contraseña equivocada")
+
+    form = AuthenticationForm()
+    return render(request, "auth/login.html", {"form": form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Saliste exitosamente")
+    return redirect("home")
+    
